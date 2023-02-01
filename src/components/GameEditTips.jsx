aa0ca2191e453def50200a70ca3674f03468a306
ghostelephant <gumpman155_2@yahoo.com>
2023-02-01 12:32:57 -0600

    Add instruction for capitals in territory array

--- src/components/GameEditTips.jsx
+++ src/components/GameEditTips.jsx
@@ -5,14 +5,22 @@ const GameEditTips = () =>
         <i className="material-icons left">chevron_right</i>
         Discord IDs can be found by right-clicking on a user's profile
       </li>
+
       <li className="col s12">
         <i className="material-icons left">chevron_right</i>
         Separate territory names with spaces, no punctuation
       </li>
+
       <li className="col s12">
         <i className="material-icons left">chevron_right</i>
         Territory names are not case sensitive
       </li>
+
+      <li className="col s12">
+        <i className="material-icons left">chevron_right</i>
+        If a nation has a capital or capitals, include them in the territories list as well (if you forget though, you'll get an error message and it'll be easy to fix)
+      </li>
+
       <li className="col s12">
         <i className="material-icons left">chevron_right</i>
         When you have finished editing, click the "Create JSON" button at the bottom
